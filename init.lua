b64ee60bc25291db9541219af18381ffdde88d0c
Marcela Rosales <marosalesji@gmail.com>
2025-05-13 12:27:32 -0600

    update
    Signed-off-by: Marcela Rosales <marosalesji@gmail.com>

--- init.lua
+++ init.lua
@@ -1,3 +1,22 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+-- Eliminar trailing whitespaces al guardar el archivo
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+})
+
+-- Set textwidth to 120 characters
+vim.opt.textwidth = 120
+
+-- Enable automatic text wrapping for markdown and text files
+vim.cmd([[
+  autocmd FileType markdown,text setlocal formatoptions+=t
+]])
+
+-- Auto format text on save for markdown and text files
+vim.cmd([[
+  autocmd BufWritePre *.md,*.txt normal! ggVGgq
+]])
--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -21,17 +21,17 @@ return {
   },
 
   -- Formateo y linting con null-ls
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local null_ls = require("null-ls")
-      opts.sources = {
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.diagnostics.ruff,
-      }
-    end,
-  },
+  --{
+  --  "jose-elias-alvarez/null-ls.nvim",
+  --  opts = function(_, opts)
+  --    local null_ls = require("null-ls")
+  --    opts.sources = {
+  --      null_ls.builtins.formatting.black,
+  --      null_ls.builtins.formatting.isort,
+  --      null_ls.builtins.diagnostics.ruff,
+  --    }
+  --  end,
+  --},
 
   -- Treesitter para mejor resaltado de sintaxis
   {
